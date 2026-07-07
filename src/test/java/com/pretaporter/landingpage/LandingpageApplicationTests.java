27f7ab83fb976ec0e25b678537d1f04aef40f9e7
ReinaMarlon <reinadinas.marlon@gmail.com>
2026-07-07 11:08:41 -0500

    Initial commit

---
+++ src/test/java/com/pretaporter/landingpage/LandingpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pretaporter.landingpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandingpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
